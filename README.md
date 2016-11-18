a7373d657afa45a46fafee211c12eae69c28b94e
trionprog <trionprog@gmail.com>
2016-11-18 06:35:01 +0300

    Example

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # sphinxad-rs
-Sphinxad hight-level wrapper, that allows to record voice from microphone and later recognize speech by pocketsphinx
+Sphinxad hight-level wrapper, that allows to record voice from microphone and later recognize the speech by pocketsphinx
 
 Usage
 -----
 
 You need two libraries to recognize speech:
 * first should record voice from microphone(this [sphinxad-rs](https://github.com/TrionProg/sphinxad-rs))
-* and second should process sound and recognize speech([pocketsphinx-rs](https://github.com/kriomant/pocketsphinx-rs))
+* and second should process sound and recognize the speech([pocketsphinx-rs](https://github.com/kriomant/pocketsphinx-rs))
 
 They are based on low-level libraries: [sphinxad-sys](https://github.com/TrionProg/sphinxad-sys) and [pocketsphinx-sys](https://github.com/kriomant/pocketsphinx-sys).
 To discover how to use this libraries, look on [examples](https://github.com/TrionProg/sphinxad-rs/tree/master/examples).
@@ -19,7 +19,7 @@ In order to use the this crate, you must have the `libpocketsphinx`, `libsphinxa
 Documentation
 -------------
 
-* [sphinxad-ad documentation](https://docs.rs/sphinxad-sys/0.1.0/sphinxad_ad/)
+* [sphinxad-rs documentation](https://docs.rs/sphinxad/0.1.0/sphinxad/)
 * [sphinxad official documentation](http://cmusphinx.sourceforge.net/doc/sphinxbase/ad_8h.html)
 
 License
---
+++ examples/record and write to file.rs
@@ -0,0 +1,111 @@
+//this is example how to record sounds from microphone
+//Add to Cargo.toml
+[dependencies]
+sphinxad = "*"
+hound = "*"
+
+//=========================
+extern crate sphinxad;
+extern crate hound;
+
+use sphinxad::AudioDevice;
+use std::io;
+use std::thread;
+use std::sync::{Arc,Mutex};
+
+const SPHINX_FREQ:u32=16000; //sphinx want 16kSPS
+
+fn record_and_write_to_file() -> io::Result<()> {
+    println!("input name of outer wav file");
+
+    let mut wavFileName = String::new();
+    io::stdin().read_line(&mut wavFileName)?;
+
+    if wavFileName.trim().len()==0 {
+        return Err(io::Error::new(io::ErrorKind::Other, "no name of outer wav file "));
+    }
+
+    //specify wav file
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: SPHINX_FREQ,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    let mut writer = hound::WavWriter::create(wavFileName.as_str().trim(), spec).unwrap();
+
+    //open Microphone
+    let mut device=AudioDevice::default()?;
+
+    loop{
+        println!("input `start` to record or `exit`");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        match input.as_str().trim(){
+            "exit" => break,
+            "start" => {
+                println!("recording...");
+
+                let shouldStop=Arc::new(Mutex::new(false));
+                let shouldStopWait=shouldStop.clone();
+
+                let handle=thread::spawn(move||{
+                    loop{
+                        println!("input `stop` to stop recording");
+
+                        let mut input = String::new();
+                        io::stdin().read_line(&mut input);
+
+                        if input.as_str().trim()=="stop" {
+                            *shouldStopWait.lock().unwrap()=true;
+                            break;
+                        }
+                    }
+                });
+
+                let mut buffer = [0i16; 2048];
+
+                //start recording
+                device.start_recording()?;
+
+                while !{*shouldStop.lock().unwrap()} {//TODO:strange behaviour, last part of sound do not record
+                    //record to buffer
+                    match device.read(&mut buffer[..])? {
+                        Some( buffer ) => {
+                            for i in 0..buffer.len() {
+                                writer.write_sample(buffer[i]).unwrap();
+                            }
+                        },
+                        None => break,
+                    }
+
+                    thread::sleep_ms(100);
+                }
+
+                //stop recording
+                device.stop_recording()?;
+
+                handle.join();
+
+                println!("stoped");
+            },
+            _=>println!("unknown command"),
+        }
+    }
+
+    writer.finalize();
+
+    Ok(())
+    //device will be closed here
+}
+
+fn main(){
+    match record_and_write_to_file(){
+        Ok ( _ ) => println!("ok"),
+        Err( e ) => println!("Error:{:?}",e),
+    }
+}
+ 
